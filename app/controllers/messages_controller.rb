aaaab1cb92aab3340fe7615c202b7223f4c3d12e
LH <lh@lhdv.net>
2020-10-08 22:10:05 -0300

    Change Chatroom#index view to save and show messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,30 @@
 class MessagesController < ApplicationController
+  
   def show
   end
+  
   def index
   end
+  
   def new
   end
+  
   def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      redirect_to root_path
+    end
   end
+  
   def edit
   end
+  
   def update
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
